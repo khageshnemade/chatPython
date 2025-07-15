87585c8cc238368bfdf4c79a0d61e796c3f6dd87
Khagesh Nemade <khageshnemade@gmail.com>
2025-07-16 05:21:52 +0530

    addeddeploy.yml

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('', views.join_chat, name='join_chat'),
-        path('api/register/', views.register_user, name='register_user'),
-    path('api/join-thread/', views.join_thread),
+        path('api/add/', views.register_user, name='register_user'),
+    path('api/join-room/', views.join_thread),
 ]
